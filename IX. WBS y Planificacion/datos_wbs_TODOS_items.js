e3381c4e84e0177bdae30d32882a097648139d20
OpenGravity <dieleozagent@gmail.com>
2026-03-13 14:14:05 +0000

    Fix: Add root index.html and sync WBS data v3.0

--- IX. WBS y Planificacion/datos_wbs_TODOS_items.js
+++ IX. WBS y Planificacion/datos_wbs_TODOS_items.js
@@ -1,3 +1,3 @@
 // WBS Datos Completos - Generado por LFC-CLI (Linux)
-// Fecha: 3/13/2026, 1:57:37 PM
+// Fecha: 3/13/2026, 2:14:05 PM
 window.datos_wbs = {"version":"3.0","fecha_actualizacion":"2026-03-13","total_capitulos":6,"items":[]};
